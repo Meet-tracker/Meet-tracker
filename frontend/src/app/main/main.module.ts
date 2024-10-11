f298e6142048ac1987b00f29a924c2ed21d0955e
Дубровский Давид <d.david-77@mail.ru>
2024-10-11 20:03:54 +0500

    компонент main

---
+++ frontend/src/app/main/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { routes } from './main.routes';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    routes
+  ]
+})
+export class MainModule { }
