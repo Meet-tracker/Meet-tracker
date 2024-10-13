af4a42fa675bb3b24d6b7a7b784646f93d72067c
Николай <91284688+imsent@users.noreply.github.com>
2024-10-13 17:09:40 +0500

    backend

--- backend/whisper/app.py
+++ backend/whisper/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import routers
 
 
@@ -6,6 +7,13 @@ app = FastAPI()
 for router in routers:
     app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="localhost:*,127.0.0.1:*".split(','),
+    allow_credentials=True,
+    allow_methods='*'.split(','),
+    allow_headers='*'.split(','),
+)
 
 if __name__ == "__main__":
     import uvicorn
